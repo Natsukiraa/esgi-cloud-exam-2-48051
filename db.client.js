80a34389d69336d6ae8da55fbe81b26b035a4a71
Natsukiraa <gazio.mael@gmail.com>
2024-04-02 14:52:58 +0200

    Forgot to remove test url

--- db.client.js
+++ db.client.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize')
 // database
 const sequelize = new Sequelize(
 
-    "postgres://dbpgsql_user:Av2D4YVHIgva9VTt347Yf9G9vI3Bxvpf@dpg-co5vp563e1ms73bd2pq0-a.frankfurt-postgres.render.com/dbpgsql",
+    process.env.DB_URL,
   {
     dialectOptions: {
       ssl: {
