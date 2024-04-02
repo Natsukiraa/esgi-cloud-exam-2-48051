a44bb986e74e039e47b96b418404d35709fbd34d
Natsukiraa <gazio.mael@gmail.com>
2024-04-02 14:43:33 +0200

    Remove //Todo

--- db.client.js
+++ db.client.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
-    process.env.DB_URL,
+
+    "postgres://dbpgsql_user:Av2D4YVHIgva9VTt347Yf9G9vI3Bxvpf@dpg-co5vp563e1ms73bd2pq0-a.frankfurt-postgres.render.com/dbpgsql",
   {
     dialectOptions: {
       ssl: {
